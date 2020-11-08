10aac9fcb3072de43f6d245fa84dbc2c3e34c156
Lucien <lucien@luciens-macbook-pro-1.home>
2020-11-08 23:17:51 +0100

    Update run_toymodel.py

--- run_toymodel.py
+++ run_toymodel.py
@@ -16,15 +16,14 @@ tfkl = tf.keras.layers
 
 mc = toymc_normal
 
-event_shape = [1]
-ncomp = 8
-nparam = 1
+ncomp = 16
+nparam = 2
 plot_low = -10.
 plot_high = 10.
-nbin = 256
-R = 200
+nbin = 512
+R = 500
 N = 512
-saved_model_path = 'saved_model/mdn_201104_01'
+saved_model_path = 'saved_model/mdn_201106_01'
 
 model = MDN(nbin,ncomp,nparam)
 optimizer = tf.keras.optimizers.Adam()
@@ -32,11 +31,15 @@ optimizer = tf.keras.optimizers.Adam()
 bins = [plot_low+ibin*(plot_high-plot_low)/nbin for ibin in range(nbin+1)]
 for r in range(R):
     means = tfd.Uniform(low=-5., high=5.).sample([N,])
-    x = tf.transpose(mc.simulate((5000,),means))
+    sigmasln = tfd.Uniform(low=-3., high=3.).sample([N,])
+    sigmas = tf.exp(sigmasln)
+    x = tf.transpose(toymc_normal((5000,),means,sigmas))
     hists = np.apply_along_axis(lambda x: np.histogram(x, bins=bins, density=1.)[0], 1, x)
+
+    pois = tf.concat([tf.expand_dims(means,axis=1),tf.expand_dims(sigmasln,axis=1),],axis=1)
     with tf.GradientTape() as tape:
         inputs = model(hists)
-        ll = -tf.math.log(model.calculate_loss(inputs,means))
+        ll = tf.math.abs(tf.math.log(model.calculate_loss(inputs,pois)))
         ll = tf.reduce_mean(ll)
     if r % 10 == 0: print("Epoch ",r,ll)
     grad = tape.gradient(ll,model.trainable_weights)
