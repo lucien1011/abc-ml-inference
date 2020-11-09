e1f6271025c793c71b9acccf362178a4eaf09768
Lucien <lucien@luciens-macbook-pro-1.home>
2020-11-09 11:11:06 +0100

    Remove ToyMC object

--- toymc.py
+++ toymc.py
@@ -2,13 +2,6 @@ import tensorflow_probability as tfp
 
 tfd = tfp.distributions
 
-class ToyMC(object):
-    def __init__(self,sampling_func):
-        self.sampling_func = sampling_func
-
-    def simulate(self,shape,poi):
-        return self.sampling_func(shape,poi)
-
 def normal_sampling_func(shape,loc,scale=1.):
     return tfd.Normal(loc=loc,scale=scale).sample(shape)
 
